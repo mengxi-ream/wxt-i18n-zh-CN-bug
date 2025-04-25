7426012d8d095ca07ce4c777b37f05d29e7baf0f
Kuiliang Zhang <z1219202167@outlook.com>
2025-04-24 18:06:27 -0700

    first commit

---
+++ wxt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
+  manifest: {
+    default_locale: "en",
+  },
+});
